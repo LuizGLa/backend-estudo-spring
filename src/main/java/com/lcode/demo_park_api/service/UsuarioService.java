d3c2cb11cd3e1c7b552df70e8f75f1b5e2be4ae2
luiz.lima <luizgustavolima200@gmail.com>
2024-06-27 12:11:31 -0300

    first commit

---
+++ src/main/java/com/lcode/demo_park_api/service/UsuarioService.java
@@ -0,0 +1,11 @@
+package com.lcode.demo_park_api.service;
+
+import com.lcode.demo_park_api.repository.UsuarioRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class UsuarioService {
+    private final UsuarioRepository usuarioRepository;
+}
