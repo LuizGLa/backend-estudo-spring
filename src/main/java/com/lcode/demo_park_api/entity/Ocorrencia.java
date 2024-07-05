5716c81c52c925807daed2e067bd74ca96d5f4fa
luiz.lima <luizgustavolima200@gmail.com>
2024-07-05 12:15:04 -0300

    Criação de entidade Ocorrência

---
+++ src/main/java/com/lcode/demo_park_api/entity/Ocorrencia.java
@@ -0,0 +1,84 @@
+package com.lcode.demo_park_api.entity;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "ocorrencias")
+@EntityListeners(AuditingEntityListener.class)
+public class Ocorrencia implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "titulo", nullable = false, unique = true, length = 100)
+    private String titulo;
+
+    @Column(name = "descricao", nullable = false, length = 255)
+    private String descricao;
+
+    @Column(name = "localizacao", nullable = false, length = 255)
+    private String localizacao;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "tipo_ocorrencia_id", nullable = false)
+    private TipoOcorrencia tipoOcorrencia;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "rua_id", nullable = false)
+    private Rua rua;
+
+    @CreatedDate
+    @Column(name = "data_criacao")
+    private LocalDateTime dataCriacao;
+
+    @LastModifiedDate
+    @Column(name = "data_atualizacao")
+    private LocalDateTime dataAtualizacao;
+
+    @CreatedBy
+    @Column(name = "criado_por")
+    private String criadoPor;
+
+    @LastModifiedBy
+    @Column(name = "modificado_por")
+    private String modificadoPor;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (!(o instanceof Ocorrencia))
+            return false;
+        Ocorrencia ocorrencia = (Ocorrencia) o;
+        return getId().equals(ocorrencia.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
+}
